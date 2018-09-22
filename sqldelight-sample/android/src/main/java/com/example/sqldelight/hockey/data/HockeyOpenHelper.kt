28388ed9dbfd466bbbfbab488df77f3e829953c4
Alec Strong <astrong@squareup.com>
2018-09-21 20:42:09 -0400

    Switch the sample to use kotlin mpp

--- sqldelight-sample/android/src/main/java/com/example/sqldelight/hockey/data/HockeyOpenHelper.kt
+++ sqldelight-sample/android/src/main/java/com/example/sqldelight/hockey/data/HockeyOpenHelper.kt
@@ -1,7 +1,7 @@
 package com.example.sqldelight.hockey.data
 
-import android.arch.persistence.db.SupportSQLiteDatabase
-import android.arch.persistence.db.SupportSQLiteOpenHelper
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
 import android.content.Context
 import com.example.sqldelight.hockey.QueryWrapper
 import com.example.sqldelight.hockey.data.PlayerVals.Position
